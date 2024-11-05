404dc4688bd5bd8d1f10e29d9c5f6b5c34ad1724
Gustavo Silva <118355959+Gustavoif12@users.noreply.github.com>
2024-11-05 08:28:11 -0400

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CadastroProduto from './componentes/cadastroproduto/cadastroProduto.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/cadastro",
+    element: <div>Tela cadastro</div>,
+  },
+  {
+    path: "/cadastro-produto",
+    element: <CadastroProduto/>,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
